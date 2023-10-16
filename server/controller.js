56582fa763c699ce6b98d44f74b6a98a4b273900
acald0 <annecaldwell2002@gmail.com>
2023-10-15 22:12:56 -0600

    step 4 completed

--- server/controller.js
+++ server/controller.js
@@ -221,5 +221,13 @@ module.exports = {
             console.log('DB seeded!')
             res.sendStatus(200)
         }).catch(err => console.log('error seeding DB', err))
-    }
+    },
+
+    getCountries: (req, res) => {
+        sequelize.query(`
+        SELECT * FROM countries
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+    },
+
+
 }
